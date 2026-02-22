cb70426520b3f5ef700b2d33ac6973d4aaf31734
Vishal Dadia <vishaldadia@Vishals-MacBook-Air.local>
2026-02-22 12:07:46 +0530

    remarks here

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -11,22 +12,38 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String status = "Checking prefs...";
+
+  @override
+  void initState() {
+    super.initState();
+    testPrefs();
+  }
+
+  Future<void> testPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final value = prefs.getInt('user_id');
+    setState(() {
+      status = "Prefs working. user_id: $value";
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        return const MaterialApp(
-          home: Scaffold(
-            body: Center(
-              child: Text("AUTH STREAM WORKING"),
-            ),
-          ),
-        );
-      },
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(status),
+        ),
+      ),
     );
   }
 }
