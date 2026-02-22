0ba734c2f360a6923d8465d4e2872a5342aae616
Vishal Dadia <vishaldadia@Vishals-MacBook-Air.local>
2026-02-22 12:32:55 +0530

    Test LoginScreen only (build 13)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
+import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,47 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AuthGate(),
-    );
-  }
-}
-
-class AuthGate extends StatefulWidget {
-  const AuthGate({super.key});
-
-  @override
-  State<AuthGate> createState() => _AuthGateState();
-}
-
-class _AuthGateState extends State<AuthGate> {
-  bool backendReady = false;
-
-  @override
-  void initState() {
-    super.initState();
-    checkPrefs();
-  }
-
-  Future<void> checkPrefs() async {
-    final prefs = await SharedPreferences.getInstance();
-    backendReady = prefs.getInt('user_id') != null;
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        return Scaffold(
-          body: Center(
-            child: Text(
-              "AuthGate OK\nfirebaseUser: ${snapshot.hasData}\nbackendReady: $backendReady",
-              textAlign: TextAlign.center,
-            ),
-          ),
-        );
-      },
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(), // 🔥 Testing LoginScreen directly
     );
   }
 }
